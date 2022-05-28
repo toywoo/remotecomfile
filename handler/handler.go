aef478d9709e60f461c51669a67d8f49d5a098a6
toywoo <39830308+toywoo@users.noreply.github.com>
2022-05-28 09:34:25 +0900

    before make makeCodeFile func

--- handler/handler.go
+++ handler/handler.go
@@ -1,15 +1,16 @@
 package handler
 
 import (
+    "crypto/sha256"
 	"encoding/json"
+	"encoding/hex"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"regexp"
 	"strconv"
 	"strings"
-
-	service "classNote/service"
 )
 
 const MAIN_URL = "http://localhost:3002"
@@ -26,8 +27,46 @@ func index(res http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func makeCodeFile(code, lang) {
+
+}
+
 // comfile
-func comfile(res http.ResponseWriter)
+func comfile(res http.ResponseWriter, req *http.Request) {
+	lang := req.FormValue("lang")
+	code := req.FormValue("code")
+
+	hash := sha256.New()
+
+	hash.Write([]byte(code))
+
+	md := hash.Sum(nil)
+	mdStr := hex.EncodeToString(md)
+
+	codeFileName := "/codes/" + mdStr[:10] + "." + ""
+    
+	codeFile, err := os.Create(codeFileName)
+    defer codeFile.Close()
+	if err != nil {
+        errCookie := http.Cookie{
+			Name:     "errorServer",
+			Value:    "err: create file",
+			SameSite: http.SameSiteLaxMode,
+		}
+		res.Header().Set("Set-Cookie", errCookie.String())
+		http.Redirect(res, req, MAIN_URL, http.StatusSeeOther)
+	}
+    fmt.Fprintf(codeFile, code)
+
+	switch lang {
+	case "py":
+		// connect py compiler
+	case "c":
+		// connect c compiler
+	case "cpp":
+		//connect cpp compiler
+	}
+}
 
 func (handler *Handler) PathNav(res http.ResponseWriter, req *http.Request) {
 	var path = req.URL.Path
